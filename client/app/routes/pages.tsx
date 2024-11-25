dc7184f8cd3af265dee4d8458c42d642e723ad0d
LoreviQ <lorevi@protonmail.com>
2024-11-25 19:08:27 +0000

    Changed pages to use a links

--- client/app/routes/pages.tsx
+++ client/app/routes/pages.tsx
@@ -1,29 +1,27 @@
 // Simple page to show expected pages within scope during development
 
-import { Link } from "@remix-run/react";
-
 export default function Pages() {
     return (
         <div className="flex flex-col items-center justify-center h-screen space-y-4">
             <h1 className="text-4xl font-bold">Pages</h1>
-            <Link to="/">
-                <p>root</p>
-            </Link>
-            <Link to="/login">
-                <p>login</p>
-            </Link>
-            <Link to="/register">
-                <p>register</p>
-            </Link>
-            <Link to="/partners">
-                <p>partners</p>
-            </Link>
-            <Link to="/subscribe">
-                <p>about</p>
-            </Link>
-            <Link to="/dashboard">
-                <p>dashboard</p>
-            </Link>
+            <a href="/" className="text-blue-400 hover:underline">
+                root
+            </a>
+            <a href="/login" className="text-blue-400 hover:underline">
+                login
+            </a>
+            <a href="/register" className="text-blue-400 hover:underline">
+                register
+            </a>
+            <a href="/partners" className="text-blue-400 hover:underline">
+                partners
+            </a>
+            <a href="/subscribe" className="text-blue-400 hover:underline">
+                about
+            </a>
+            <a href="/dashboard" className="text-blue-400 hover:underline">
+                dashboard
+            </a>
         </div>
     );
 }
